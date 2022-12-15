2ac77682ccb730e4905b177701e15793463e20ec
minkman <lomick2090@gmail.com>
2022-12-15 12:22:51 -0600

    Add team photos

--- src/createAboutUs.js
+++ src/createAboutUs.js
@@ -1,19 +1,27 @@
 export function createAboutUs(content) {
 
-    let contentDiv = document.createElement('div');
-    contentDiv.className = 'contentDiv'
+    let aboutUsDiv = document.createElement('div');
+    aboutUsDiv.className = 'aboutUsDiv'
 
     let aboutDiv = document.createElement('div');
     aboutDiv.className = 'aboutDiv';
-    aboutDiv.textContent = `Bella's Bistro is the best Italian fine dining establishment you can find in Putnam, CT.
+
+    let aboutTitle = document.createElement('h2');
+    aboutTitle.textContent = 'Who Are We?';
+    aboutDiv.appendChild(aboutTitle);
+   
+
+    let aboutContent = document.createElement('div');
+    aboutContent.textContent = `Bella's Bistro is the best Italian fine dining establishment you can find in Putnam, CT.
     Every item on the menu has been carefully crafted by Albert Grandburger, the owner and 
     head chef of Bella's Bistro. With a master's in fine cuisine from Italy's top cooking 
     school and 20 years of kitchen cooking experience, Albert has brought every thing he's 
     learned together for this high quality experience.`
+    aboutDiv.appendChild(aboutContent);
 
     let hoursDiv = document.createElement('ul');
     hoursDiv.className = 'hoursDiv';
-    let hoursTitle = document.createElement('h3');
+    let hoursTitle = document.createElement('h2');
     hoursTitle.textContent = 'Hours:'
     hoursTitle.style = 'text-align: center; margin-top: 0px;';
     let li1 = document.createElement('li');
@@ -33,11 +41,35 @@ export function createAboutUs(content) {
     hoursDiv.appendChild(li3);
     hoursDiv.appendChild(li4);
     hoursDiv.appendChild(li5);
+
+
+    let teamDiv = document.createElement('div');
+    teamDiv.className = 'teamDiv';
+    let teamTitle = document.createElement('h2');
+    teamTitle.textContent = 'Meet Our Team:'
+    teamDiv.appendChild(teamTitle);
+    let teamCards = document.createElement('div');
+    teamCards.className = 'teamCards';
+
+    for (let i=1; i<=9; i++) {
+        let newTeamCard = document.createElement('div');
+        newTeamCard.id = i;
+        let newTeamImg = document.createElement('img');
+        newTeamImg.src = `./${i}.jpg`;
+        let newTeamAbout = document.createElement('div');
+        newTeamCard.appendChild(newTeamImg);
+        newTeamCard.appendChild(newTeamAbout);
+        teamCards.appendChild(newTeamCard);
+    }
+
+    teamDiv.appendChild(teamCards);
+
     
 
 
 
-    contentDiv.appendChild(aboutDiv)
-    contentDiv.appendChild(hoursDiv)
-    content.appendChild(contentDiv); 
+    aboutUsDiv.appendChild(aboutDiv);
+    aboutUsDiv.appendChild(hoursDiv);
+    aboutUsDiv.appendChild(teamDiv);
+    content.appendChild(aboutUsDiv); 
 }
